72828b57e078dcded37448ed2540a01afe640a30
Samar Abbas <samarabbas172003@gmail.com>
2024-06-26 18:12:48 +0530

    two sum

---
+++ ARRAY/Leaders.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v = {7, 6, 5, 4, 3, 2, 3, 2, 1};
+    vector<int> ans;
+
+    int j = 0;
+    for(int i=0;i<v.size();i++)
+    {
+  
+    }
+   
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+
+    return 0;
+}
---
+++ ARRAY/TwoSum.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    vector<pair<int,int>> temp;
+    for(int i=0;i<nums.size();i++){
+        temp.push_back({nums[i],i});
+    }
+    
+    sort(temp.begin(), temp.end());
+
+    int left = 0;
+    int right = temp.size() - 1;
+    while (left < right)
+    {
+        if ((temp[left].first + temp[right].first) == target)
+           return {temp[left].second,temp[right].second};
+
+        else if ((temp[left].first + temp[right].first) < target)
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+    
+   
+    return {};
+}
+
+int main()
+{
+
+    vector<int> v;
+    v = {1,2,3,4,5};
+    vector<int> result = twoSum(v, 5);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i] << " ";
+    }
+    return 0;
+}
