cea56f278f94d3b39e68e0ed3e169f3a0c8b2fd6
Samar Abbas <samarabbas172003@gmail.com>
2024-08-13 23:57:19 +0530

    BFS

---
+++ LINKED_LIST/OddEven.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+int main()
+{
+
+    return 0;
+}
